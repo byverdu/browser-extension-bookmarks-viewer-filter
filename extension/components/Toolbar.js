20eb521891e5345c4e5ad558ad8499f0887898d5
Albert Vallverdu <byverdu@gmail.com>
2024-12-21 23:20:14 +0000

    refactor(SortBtn): Replace usage of innerHtml with template

--- extension/components/Toolbar.js
+++ extension/components/Toolbar.js
@@ -1,45 +1,40 @@
-export class SortButtons extends HTMLElement {
+export class SortButton extends HTMLElement {
   constructor() {
     super();
+    const template = document.getElementById('sort-button-template');
+    const content = template.content.cloneNode(true);
+
+    this.appendChild(content);
   }
 
-  get sort() {
-    return this.parentElement.getAttribute('sort');
+  get sortDescending() {
+    return this.parentElement.getAttribute('sort') === 'desc';
   }
 
-  get disabled() {
-    return this.parentElement.getAttribute('disabled');
+  get isDisabled() {
+    return this.parentElement.getAttribute('disabled') === 'true';
   }
 
   connectedCallback() {
-    this.render();
+    const button = this.querySelector('button');
+    const icon = this.querySelector('i');
+    const sortIcon = this.sortDescending
+      ? 'fa-sort-numeric-down'
+      : 'fa-sort-numeric-up';
 
-    this.querySelectorAll('button').forEach(button => {
-      button.addEventListener('click', this.clickHandler.bind(this));
-    });
+    if (!button || !icon) return;
+
+    button.addEventListener('click', this.clickHandler.bind(this));
+    button.toggleAttribute('disabled', this.isDisabled);
+
+    icon.classList.add(sortIcon);
   }
 
   clickHandler() {
-    const sort = this.sort === 'desc' ? 'asc' : 'desc';
+    const newSort = this.sortDescending ? 'asc' : 'desc';
 
-    document.getElementById('search-results').setAttribute('sort', sort);
-    this.parentElement.setAttribute('sort', sort);
-  }
-
-  render() {
-    const disabled = this.disabled === 'true' ? 'disabled' : '';
-    const sort =
-      this.sort === 'desc' ? 'fa-sort-numeric-down' : 'fa-sort-numeric-up';
-
-    this.innerHTML = `
-      <p class="control">
-        <button ${disabled} data-sort="desc" class="button" title="Sort Descending">
-          <span class="icon">
-            <i class="fas ${sort}"></i>
-          </span>
-        </button>
-      </p>
-    `;
+    document.getElementById('search-results').setAttribute('sort', newSort);
+    this.parentElement.setAttribute('sort', newSort);
   }
 }
 
--- extension/pages/options.js
+++ extension/pages/options.js
@@ -1,11 +1,11 @@
 import {
   SearchBox,
   SearchResults,
-  SortButtons,
+  SortButton,
   Toolbar,
 } from '../components/index.js';
 
 customElements.define('x-search-results', SearchResults);
 customElements.define('x-search-box', SearchBox);
 customElements.define('x-toolbar', Toolbar);
-customElements.define('x-sort-buttons', SortButtons);
+customElements.define('x-sort-button', SortButton);
